ace7c55e7b0a17934c907d58fd7dfbe0e4301359
Lautaro Faustino <lautarofaustino12@gmail.com>
2022-02-10 13:02:14 -0300

    promo de 2*1 de cocas

--- descuentos.py
+++ descuentos.py
@@ -1,31 +1,36 @@
 
 # Precios
-huevos_precio=float(input("Ingresar el precio del Maple de Huevos: ")) 
-paleta_precio=float(input("Ingresar el precio del Kilo de Paleta: "))
+coca_precio=float(input("Ingresar el precio de botellas de coca: ")) 
+ricota_precio=float(input("Ingresar el precio del Kilo de ricota: "))
 
 # Cantidades
-huevos_cant_maples=float(input("Ingresar cantidad de Maples de Huevos: "))
-paleta_cant_kilo=float(input("Ingresar peso de Paleta: "))
+coca_cant_botellas=float(input("Ingresar cantidad de botellas de coca: "))
+ricota_cant_kilo=float(input("Ingresar peso de ricota: "))
+
+#calculo de 2 x 1 
+beneficios =int(coca_cant_botellas/2)
+sobra=coca_cant_botellas-(beneficios*2)
+coca_subtotal=coca_precio*(beneficios+sobra)
 
 # calculo de subtotales
-huevos_subtotal=huevos_precio * huevos_cant_maples
-paleta_subtotal=paleta_precio * paleta_cant_kilo 
+ricota_subtotal=ricota_precio * ricota_cant_kilo 
 
 # calculo del total
-total=paleta_subtotal+huevos_subtotal 
+total=ricota_subtotal+coca_subtotal 
 
 # Calculo reintegro del 30%
 reintegro_30=total*0.3
 
 # Calculo descuento del 20%
-descuento_20=paleta_subtotal*0.2
+descuento_35=ricota_subtotal*0.2
 
-total_con_descuento=total-descuento_20
+total_con_descuento=total-descuento_35
 
-print("Subtotal Huevos: "+str(huevos_subtotal)+"$")
-print("Subtotal Paleta: "+str(paleta_subtotal)+"$")
+
+print("Subtotal cocas: "+str(coca_subtotal)+"$")
+print("Subtotal ricota: "+str(ricota_subtotal)+"$")
 print("Total sin descuento: "+str(total)+"$")
 print("Reintegro del 30%: "+str(reintegro_30)+"$")
-print("Descuento del 20%: "+str(descuento_20)+"$")
+print("Descuento del 20%: "+str(descuento_35)+"$")
 
 print("Total con descuento: "+str(total_con_descuento)+"$")
