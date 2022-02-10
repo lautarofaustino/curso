6b6d29fe5203837701482400df31e797a96ea026
Lautaro Faustino <lautarofaustino12@gmail.com>
2022-02-10 10:18:21 -0300

    Carga inicial.

---
+++ descuentos.py
@@ -0,0 +1,16 @@
+
+# Lista de precios
+huevos=300
+paleta=1000
+
+# Calculo reintegro del 30%
+reintegro_30=(huevos+paleta)*0.3
+
+# Calculo descuento del 20%
+descuento_20=paleta-(paleta*0.2)
+
+print("Huevos: "+str(huevos)+"$")
+print("Paleta: "+str(paleta)+"$")
+print("Reintegro del 30%: "+str(reintegro_30)+"$")
+print("Descuento del 20%: "+str(descuento_20)+"$")
+
